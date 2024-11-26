0a87dc5675c113fe04d9d6e35ab87a459611e6d5
Vincent <vincent.granie@2018.icam.fr>
2024-11-26 09:30:47 +0100

    project setup

--- src/app/transfer/page.tsx
+++ src/app/transfer/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Page() {
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
-      Login screen
+      dApp transfer page
     </div>
   );
 }
